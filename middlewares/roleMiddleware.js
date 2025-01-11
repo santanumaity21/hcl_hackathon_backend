d3b09866ae19ed986f5c17cf447182aafa8bf67e
Avinash Srivastava <avinash@threado.com>
2025-01-11 12:45:53 +0530

    INIT

--- middlewares/roleMiddleware.js
+++
@@ -1,8 +0,0 @@
-const roleMiddleware = (roles) => (req, res, next) => {
-    if (!req.user || !roles.includes(req.user.role)) {
-        return res.status(403).json({ error: 'Access denied' });
-    }
-    next();
-};
-
-module.exports = { roleMiddleware };
